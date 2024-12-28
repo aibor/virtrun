658e79397ed8f5f99c65ea18af22ffe5edd25299
Tobias Böhm <code@aibor.de>
2024-12-28 21:21:30 +0100

    refactor(virtfs): rename initramfs package
    The initramfs package only contains the generic virtual FS builder.
    
    This commit renames the package to virtfs which is more appropriate for
    the functionality it provides.

--- internal/initramfs/doc.go
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: 2024 Tobias Böhm <code@aibor.de>
-//
-// SPDX-License-Identifier: GPL-3.0-or-later
-
-// Package initramfs can beused to build simple initramfs CPIO archives. It is
-// intended for short lived guests only. The initramfs archives is supposed to
-// be as small as possible with only a couple of binaries and their required
-// shared libraries.
-package initramfs
